c522f56a77c2647eeecd1016277ce908b72f9a52
jacek <jw>
2016-08-07 16:43:04 +0200

    tmp

--- app.js
+++ app.js
@@ -17,27 +17,32 @@ router.route("/event")
         Event.find({}, function (err, data) {
 
             if (err) {
-                response = {"error": true, "message": "Error fetching data"};
+                response = {"error": true, "message": "Error fetching data" + err};
             } else {
                 response = {"error": false, "message": data};
             }
             res.json(response);
         });
-    }).post(function (req, res) {
-    var event = new Event();
-    event.name = req.body.name;
-    event.accepted = req.body.accepted;
+    })
+    .post(function (req, res) {
+        //TODO: walidacja? czy mongoose wystarczy?
+        var event = new Event();
+        event.name = req.body.name;
+        event.creator = req.body.creator;
+        //dodanie twócy
+        event.accountsConfirmed = [];
+        event.accountsConfirmed.push(req.body.creator)
 
-    event.save(function (err) {
-        var response = {};
-        if (err) {
-            response = {"error": true, "message": "Error adding data"};
-        } else {
-            response = {"error": false, "message": "Event added"};
-        }
-        res.json(response);
+        event.save(function (err) {
+            var response = {};
+            if (err) {
+                response = {"error": true, "message": "Error adding data" + err};
+            } else {
+                response = {"error": false, "message": "Event added"};
+            }
+            res.json(response);
+        });
     });
-});
 
 router.route("/event/:id")
     .get(function (req, res) {
@@ -69,7 +74,7 @@ router.route("/event/:id")
 
                 data.save(function (err) {
                     if (err) {
-                        response = {"error": true, "message": "Error updating data"};
+                        response = {"error": true, "message": "Error updating data" + err};
                     } else {
                         response = {"error": false, "message": "Data is updated for " + req.params.id};
                     }
@@ -77,7 +82,107 @@ router.route("/event/:id")
                 })
             }
         });
+    });
+
+router.route("/event/:id/status")
+    .get(function (req, res) {
+        var response = {};
+
+        Event.findById(req.params.id, function (err, data) {
+            if (err) {
+                response = {"error": true, "message": "Error fetching data"};
+            } else {
+                response = {"error": false, "status": data.status};
+            }
+            res.json(response);
+        });
     })
+    .put(function (req, res) {
+        var response = {};
+
+        Event.findById(req.params.id, function (err, data) {
+            if (err) {
+                response = {"error": true, "message": "Error fetching data"};
+            } else {
+                if (req.body.status !== undefined) {
+                    data.status = req.body.status;
+                }
+
+                data.save(function (err) {
+                    if (err) {
+                        response = {"error": true, "message": "Error updating data" + err};
+                    } else {
+                        response = {"error": false, "message": "Data is updated for " + req.params.id};
+                    }
+                    res.json(response);
+                })
+            }
+        });
+    });
+
+
+router.route("/event/:id/account/:name")
+    .delete(function (req, res) {
+        var response = {};
+
+        Event.findById(req.params.id, function (err, data) {
+            if (err) {
+                response = {"error": true, "message": "Error fetching data"};
+            } else {
+                //usunięcie z listy jeśli na niej jest
+                if (typeof data.accountsRejected == "undefined"
+                    || data.accountsRejected == null) {
+                    data.accountsRejected = [];
+                }
+
+                if (data.accountsRejected.indexOf(req.params.name) != -1) {
+                    data.accountsRejected.splice(data.accountsRejected.indexOf(req.params.name), 1)
+                }
+
+                data.save(function (err) {
+                    if (err) {
+                        response = {"error": true, "message": "Error updating data" + err};
+                    } else {
+                        response = {"error": false, "name": req.params.name};
+                    }
+                    res.json(response);
+                })
+            }
+        });
+    })
+    .post(function (req, res) {
+        var response = {};
+
+        Event.findById(req.params.id, function (err, data) {
+            if (err) {
+                response = {"error": true, "message": "Error fetching data"};
+
+                res.json(response);
+            } else if (data != null) {
+                if (typeof data.accountsConfirmed == "undefined"
+                    || data.accountsConfirmed == null) {
+                    data.accountsConfirmed = [];
+                }
+
+                if (data.accountsConfirmed.indexOf(req.params.name) == -1) {
+                    data.accountsConfirmed.push(req.params.name)
+                }
+
+                data.save(function (err) {
+                    if (err) {
+                        response = {"error": true, "message": "Error updating data" + err};
+                    } else {
+                        response = {"error": false, "message": "Data is updated for " + req.params.name};
+                    }
+                    res.json(response);
+                })
+            } else {
+                response = {"error": true, "message": "No data with id " + req.params.id};
+                res.status(404).json(response);
+            }
+
+        });
+    });
 
 
 app.use('/', router);
