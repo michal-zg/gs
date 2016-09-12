af502456c34517e12012c25e881eeba7797f36f6
jacek <jw>
2016-09-12 23:05:05 +0200

    -notifications endpoint\ -wprowadzenie promises

--- gs-backend/models/IEvent.js
+++
@@ -1,6 +0,0 @@
-var EventStatus;
-(function (EventStatus) {
-    EventStatus[EventStatus['new'] = 0] = 'new';
-    EventStatus[EventStatus['canceled'] = 1] = 'canceled';
-})(EventStatus || (EventStatus = {}));
-//# sourceMappingURL=IEvent.js.map
--- gs-backend/models/Schema.js
+++ gs-backend/models/Schema.js
@@ -1,3 +1,4 @@
+"use strict";
 var mongoose = require("mongoose");
 var mongoDbIp = 'localhost';
 mongoose.connect('mongodb://' + mongoDbIp + ':27017/game-scheduler');
